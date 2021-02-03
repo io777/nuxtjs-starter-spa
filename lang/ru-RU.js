a316daddf1cc360264829703118b72cf1d9f1043
io777 <Shap.production@mail.ru>
2021-02-03 15:28:12 +0500

    some change

---
+++ lang/ru-RU.js
@@ -0,0 +1,10 @@
+// ru-RU.js с русским переводом
+export default {
+  hello: 'Привет!',
+  route: {
+
+  },
+  pages: {
+
+  }
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import webpack from 'webpack'
+// const axios = require('axios')
 
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
@@ -13,7 +14,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'keywords', name: 'keywords', content: '' },
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:title', property: 'og:title', content: '' },
+      { hid: 'og:description', property: 'og:description', content: '' },
+      { hid: 'og:image', property: 'og:image', content: 'https://site.ru/banner.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -22,10 +27,12 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    'bootstrap/dist/css/bootstrap.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~plugins/bootstrap.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -34,12 +41,57 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/moment'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@nuxtjs/axios',
+    ['nuxt-i18n', {
+      detectBrowserLanguage: {
+        useCookie: true,
+        cookieKey: 'i18n_redirected',
+        alwaysRedirect: false,
+        fallbackLocale: 'ru'
+      },
+      locales: [
+        {
+          name: 'Russia',
+          code: 'ru',
+          iso: 'ru-RU',
+          file: 'ru-RU.js'
+        },
+        {
+          name: 'English',
+          code: 'en',
+          iso: 'en-US',
+          file: 'en-US.js'
+        }
+      ],
+      lazy: true,
+      langDir: 'lang/',
+      defaultLocale: 'ru'
+    }],
+    // ['@nuxtjs/yandex-metrika',
+    //   {
+    //     id: '62051137',
+    //     webvisor: true,
+    //     clickmap:true,
+    //     // useCDN:false,
+    //     trackLinks:true,
+    //     accurateTrackBounce:true,
+    //   }
+    // ],
+    // ['@nuxtjs/google-analytics', {
+    //   id: 'UA-164544504-1'
+    // }],
+    '@nuxtjs/sitemap'
   ],
+  sitemap: {
+    hostname: 'https://',
+    routes: []
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
